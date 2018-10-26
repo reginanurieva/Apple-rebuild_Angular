96630e947af3dd5383c3c76124f3483bb6cd104a
Regina Nurieva <regginurieva@gmail.com>
2018-10-26 13:40:54 -0700

    add cards component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { IphoneComponent } from './iphone/iphone.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    IphoneComponent
+    IphoneComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
